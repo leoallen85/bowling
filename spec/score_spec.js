aecc8600075b950701f75994221e6d99d70e1194
Leo Allen <leo.f.allen@gmail.com>
2016-02-29 13:25:43 +0000

    Working frame and game.
    I removed the spies as they weren't really adding anything

--- spec/score_spec.js
+++ spec/score_spec.js
@@ -2,20 +2,37 @@ var Game = require('../lib/game');
 
 describe("A game", function() {
 
-  it("calculates a game's score", function() {
-    game = new Game(generateFrames());
+  var game;
+
+  beforeEach(function(){
+    game = new Game();
+  });
+
+  it("calculates a gutter game", function() {
+    generateFrames([0,0]);
+    expect(game.score()).toEqual(0);
+  });
+
+  it("calculates a standard game", function() {
+    generateFrames([1,3]);
+    expect(game.score()).toEqual(40);
+  });
+
+  it("calculates a game with spares", function() {
+    generateFrames([5,5], [5,5,5]);
     expect(game.score()).toEqual(150);
   });
 
-  function generateFrames() {
-    var frames = [];
+  it("calculates a game with strikes", function() {
+    generateFrames([10], [10,10,10]);
+    expect(game.score()).toEqual(300);
+  });
 
-    for(i = 0; i < 10; i++) {
-      var frame = jasmine.createSpyObj('frame', ['total']);
-      frame.total.andReturn(15);
-      frames.push(frame);
+  function generateFrames(frame, final_frame) {
+    for(i = 0; i < 9; i++) {
+      game.roll(frame);
     }
 
-    return frames;
+    game.roll(final_frame || frame);
   }
 });
